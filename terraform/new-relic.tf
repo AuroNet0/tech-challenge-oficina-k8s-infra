f4795fd42d69fcdb0dc5dae20efda9fc7bd2c0e2
AuroNet0 <auroneto04@gmail.com>
2026-09-12 19:09:07 -0300

    Fix memory-problem

--- terraform/new-relic.tf
+++ terraform/new-relic.tf
@@ -22,6 +22,72 @@ resource "helm_release" "new_relic" {
 
       "newrelic-infrastructure" = {
         enabled = true
+
+        kubelet = {
+          kubelet = {
+            resources = {
+              limits = {
+                memory = "256Mi"
+              }
+              requests = {
+                cpu    = "50m"
+                memory = "64Mi"
+              }
+            }
+          }
+
+          agent = {
+            resources = {
+              limits = {
+                memory = "256Mi"
+              }
+              requests = {
+                cpu    = "50m"
+                memory = "64Mi"
+              }
+            }
+          }
+        }
+
+        ksm = {
+          ksm = {
+            resources = {
+              limits = {
+                memory = "256Mi"
+              }
+              requests = {
+                cpu    = "50m"
+                memory = "64Mi"
+              }
+            }
+          }
+
+          forwarder = {
+            resources = {
+              limits = {
+                memory = "256Mi"
+              }
+              requests = {
+                cpu    = "50m"
+                memory = "64Mi"
+              }
+            }
+          }
+        }
+      }
+
+      "nri-metadata-injection" = {
+        enabled = true
+
+        resources = {
+          limits = {
+            memory = "64Mi"
+          }
+          requests = {
+            cpu    = "25m"
+            memory = "20Mi"
+          }
+        }
       }
 
       ksm = {
@@ -30,6 +96,16 @@ resource "helm_release" "new_relic" {
 
       "kube-state-metrics" = {
         enabled = true
+
+        resources = {
+          limits = {
+            memory = "128Mi"
+          }
+          requests = {
+            cpu    = "25m"
+            memory = "48Mi"
+          }
+        }
       }
     }),
   ]
